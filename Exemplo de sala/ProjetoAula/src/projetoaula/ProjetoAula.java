df9b1857f31f2471c17d50fb8003d0ccc13e03b3
Camilly Barbosa Santos <camillybarbosasantos@outlook.com>
2025-10-28 21:21:22 -0300

    .
    Exemplo de sala para fazer a interface

---
+++ Exemplo de sala/ProjetoAula/src/projetoaula/ProjetoAula.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projetoaula;
+
+/**
+ *
+ * @author alunocmc
+ */
+public class ProjetoAula {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    public static boolean checkLogin(String usuario, String senha){
+    
+        return usuario.equals("Admin") && senha.equals("123");
+    }
+    
+}
